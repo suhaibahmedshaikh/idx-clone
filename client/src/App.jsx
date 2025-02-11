27e9b3285e9c878091f1ae81fd0e7a4e8794cc0e
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2025-02-11 21:50:59 +0530

    Added react-query for api queries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import usePing from "./hooks/apis/queries/usePing";
 
 function App() {
+  const { isLoading, data } = usePing();
+
+  if (isLoading) {
+    return <>Loading.....</>;
+  }
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello {data.message}</h1>
     </>
   );
 }
